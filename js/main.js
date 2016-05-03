387d47bd2ef7a16ea79eb3598e5a8c93e7897296
mschaefer123 <mschaefer123@Matthews-MacBook-Pro.local>
2016-05-02 22:24:22 -0700

    FINAL - Final Project | Progress Check
    added html and high-level JS pseudo code

---
+++ js/main.js
@@ -0,0 +1,72 @@
+// 16.05.13 | General Assembly FEWD | Final Project | Pinterest e-comm checkout prototype | Matt Schaefer 
+
+// ============================================
+// JS Feature List
+// ============================================
+
+// ============================================
+// Page Load
+// ============================================
+
+// Hide input labels
+
+// Hide Submit button
+
+// ============================================
+// Input Functions
+// ============================================
+
+// On click:
+	// Show label
+	// Hide placeholder text
+	// AddClass .highlight in CSS
+		// if no text in input then 
+			// hide label
+			// show placeholder text
+
+// if click other field or tab then
+	// hideClass .highlight in CSS
+
+// ============================================
+// Shipping Form
+// ============================================
+
+// on "Save Shipping" button click: 
+	// Change button text to "Edit Shipping" 
+	// SlideUp shipping fieldset:
+		// Hide input box styling
+		// Hide privacy policy text
+		// Add checkmark icon to top right of feildset
+		// Add first and last name, Address 1, City, State, Zip user input text to Review Form
+
+// ============================================
+// Payment Form
+// ============================================
+
+// Hide class .billingInputs and display .shippingInputs class (user input from shipping inputs) when user ticks .shippingCheckbox
+
+// on "Save Payment" button click: 
+	// Change button text to "Edit Payment" 
+	// SlideUp payment fieldset:
+		// Hide input box styling
+		// Hide privacy policy text
+		// Add checkmark icon to top right of feildset
+
+// ============================================
+// Review Form
+// ============================================
+
+// If shipping and Payment form inputs are completed then display Submit button 
+
+// Convert .quantity user input to a number
+	// Multiply by 200
+	// Display in sub total column
+
+// Convert estimated tax ($5.00) into a number
+// Convert shipping selection options into numbers
+	// Display user selection in Shipping column
+
+// Add .quantity, estimated tax, and shipping and display total # in Total column
+
+// Display user input for first and last name from shipping fieldset
+
